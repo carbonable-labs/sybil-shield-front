ed87a93a35cf1b5a492e38d5dc5881a811dd7861
François Schuers <francois.schuers@gmail.com>
2023-02-03 14:58:59 +0200

    feat: make data dynamic

--- app/components/Graph.tsx
+++ app/components/Graph.tsx
@@ -1,15 +1,27 @@
-import { ForceGraph3D } from "react-force-graph";
+import { ForceGraph2D, ForceGraph3D } from "react-force-graph";
 import { getColorByPercentage } from "~/utils/graphUtils";
 
-export default function Graph({ graphData }: { graphData: any }) {
-    if (graphData.hasOwnProperty("nodes")) { 
+export default function Graph({ graphData, threshold, graphStyle, contract }: { graphData: any, threshold: number, graphStyle: boolean, contract: string }) {
+    if (graphData.hasOwnProperty("nodes") && graphStyle) { 
         return (
             <div className="p-2">
                 <ForceGraph3D 
                     graphData={graphData}
-                    linkWidth={2}
                     backgroundColor="#13151C"
-                    nodeColor={(node: any) => node.val < 50 ? "#FFF" : getColorByPercentage(node.val)}
+                    nodeColor={(node: any) => node.score < (threshold/100) ? "#FFF" : getColorByPercentage(node.score)}
+                />
+            </div>
+        );
+    }
+
+    if (graphData.hasOwnProperty("nodes") && !graphStyle) { 
+        return (
+            <div className="p-2">
+                <ForceGraph2D 
+                    graphData={graphData}
+                    backgroundColor="#13151C"
+                    nodeColor={(node: any) => node.score < (threshold/100) ? "#FFF" : getColorByPercentage(node.score)}
+                    linkColor={(link: any) => "#363840"}
                 />
             </div>
         );
