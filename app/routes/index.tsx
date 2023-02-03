1fd63ebb93b7dbfea880b3b0d36b342f51a1f7f7
François Schuers <francois.schuers@gmail.com>
2023-02-03 15:47:16 +0200

    feat: copy list of wallets with score to clipboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,6 +6,8 @@ import Contract from "~/components/Contract";
 import Header from "~/components/Header";
 import Settings from "~/components/Settings/Settings";
 import SettingsMobile from "~/components/Settings/SettingsMobile";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CheckCircleIcon, ClipboardIcon } from "@heroicons/react/24/outline";
 
 let Graph = lazy(() => import("~/components/Graph"));
 
@@ -15,6 +17,16 @@ export default function Index() {
   const [threshold, setThreshold] = useState(50);
   const [graphStyle, setGraphStyle] = useState(false);
   const [contract, setContract] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  }
+
+
 
   const toggleMenu = useCallback(() => setMenuOpen(!menuOpen), [setMenuOpen, menuOpen]);
 
@@ -37,6 +49,23 @@ export default function Index() {
           <ClientOnly>
             <Suspense fallback="">
               <Graph graphData={graphData} threshold={threshold} graphStyle={graphStyle} contract={contract} />
+              { graphData.hasOwnProperty("nodes") &&
+                <CopyToClipboard text={JSON.stringify(graphData.nodes)} onCopy={handleCopy}>
+                  <div>
+                  { !copied && <button className="absolute right-2 bottom-6 inline-flex items-center justify-center uppercase text-neutral-100 outine-none rounded-full px-3 py-2 bg-greenish-500 text-center hover:bg-greenish-400" onCopy={handleCopy}>
+                    <ClipboardIcon className="w-6 h-6 mr-2" />
+                    Copy data to clipboard
+                  </button>
+                  }
+                  { copied && <button className="absolute right-2 bottom-6 inline-flex items-center justify-center uppercase text-neutral-100 outine-none rounded-full px-3 py-2 bg-greenish-500 text-center hover:bg-greenish-400" onCopy={handleCopy}>
+                    <CheckCircleIcon className="w-6 h-6 mr-2" />
+                    Copied
+                  </button>
+                  }
+                  </div>
+                  
+                </CopyToClipboard>
+              }
             </Suspense>
           </ClientOnly>
         </div> 
@@ -53,3 +82,4 @@ export function ClientOnly({ children }: { children: ReactNode }) {
   }, []);
   return mounted ? <>{children}</> : null;
 }
+
