b03cd7064ea66ae024fe289b37fe3dae32e4a30e
François Schuers <francois.schuers@gmail.com>
2023-02-04 12:22:13 +0200

    feat: new logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ toggleMenu, menuOpen }: { toggleMenu: () => voi
   return (
     <header className="w-full uppercase font-press-start font-bold text-xl md:text-3xl px-2 py-2 inline-flex items-center justify-between lg:px-4 border-b border-b-neutral-700">
         <h1 className="ml-0 inline-flex items-center justify-start lg:justify-center">
-            <img src="/assets/logo.png" className="w-12 mr-2 lg:w-16 " alt="Sybil Shield Logo" />
+            <img src="/assets/logo.png" className="w-12 mr-4 lg:w-16 " alt="Sybil Shield Logo" />
             Sybil Shield
         </h1>
         { isMobile && <DisplayIcon toggleMenu={toggleMenu} menuOpen={menuOpen} /> }
